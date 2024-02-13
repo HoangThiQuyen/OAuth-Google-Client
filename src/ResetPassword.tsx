6cdd42d1513330d483d1ea3ef14bb45d817912c7
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2024-02-13 23:13:57 +0700

    edit text

--- src/ResetPassword.tsx
+++ src/ResetPassword.tsx
@@ -4,5 +4,5 @@ import { useLocation } from "react-router-dom";
 export default function ResetPassword() {
   const location = useLocation();
   console.log(location.state);
-  return <div>Reset password</div>;
+  return <div>Form Reset password</div>;
 }
